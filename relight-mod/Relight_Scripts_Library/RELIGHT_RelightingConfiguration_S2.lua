b2680e2e9b1b6c101c1782e5ad7ca0adb9bddcd4
David Matos <40375502+frostbone25@users.noreply.github.com>
2024-08-09 18:46:11 -0500

    season 2 external lua relight script support

--- relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S2.lua
+++ relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S2.lua
@@ -9,27 +9,220 @@ RELIGHT_GenerateNewINI_Season2 = function(number_configurationVersion)
         {
             ConfigurationVersion = number_configurationVersion,
         },
------------------------------------ SEASON 2 EPISODE 1 -----------------------------------
------------------------------------ SEASON 2 EPISODE 1 -----------------------------------
------------------------------------ SEASON 2 EPISODE 1 -----------------------------------
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
+        Level_201_BackwoodsStream = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_CabinExterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_CabinInterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_CabinShedInterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         Level_201_ClearingCampfire = 
         {
-            RelightSceneLuaFile = nil,
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
         },
------------------------------------ SEASON 2 EPISODE 2 -----------------------------------
------------------------------------ SEASON 2 EPISODE 2 -----------------------------------
------------------------------------ SEASON 2 EPISODE 2 -----------------------------------
-
------------------------------------ SEASON 2 EPISODE 4 -----------------------------------
------------------------------------ SEASON 2 EPISODE 4 -----------------------------------
------------------------------------ SEASON 2 EPISODE 4 -----------------------------------
-
------------------------------------ SEASON 2 EPISODE 5 -----------------------------------
------------------------------------ SEASON 2 EPISODE 5 -----------------------------------
------------------------------------ SEASON 2 EPISODE 5 -----------------------------------
+        Level_201_RiverCamp = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_RiverShore = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_RiverWoods = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_201_TruckStopBathroom = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
+        Level_202_CabinInterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LakeBridge = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LakeBridgeApproach = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LakeBridgeStationHouse = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LakeWoodsCellar = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LakeWoodsClearing = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LodgeExteriorAndMainRoom = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LodgeExteriorFront = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LodgeExteriorRear = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_202_LodgeMainRoom = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
+        Level_203_HardwareStoreExterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreExteriorForest = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreExteriorRear = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreExteriorRoofAndYard = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreExteriorYardAfternoon = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreExteriorYardNight = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreInteriorExteriorNight = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreInteriorExteriorRoofNight = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreInteriorMain = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_203_HardwareStoreInteriorOffice = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
+        Level_204_CivilWarMemorialCenter = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_CivilWarMemorialCenterMorning = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_CivilWarMemorialMuseum = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_CivilWarMemorialObservationDeck = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_CivilWarMemorialObservationDeckEvening = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_HardwareStoreExteriorRear = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_MobileHome = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_RoadSnowy = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_204_TrailerPark = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
+        --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
+        Level_205_FinaleHardwareStore = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_FinaleOpenField = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_FinaleWellington = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_FrozenLake = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_RestStop = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_RoadSnowy = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_RoadTruckSnowstorm = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         Level_205_SnowyPowerStation = 
         {
-            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_205_SnowyPowerStation.lua",
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/RELIGHT_205_adv_snowyPowerStation.lua",
+        },
+        Level_205_SnowyWoods = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_StashHouseExterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_StashHouseExteriorNight = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_205_StashHouseInterior = 
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
         },
     };
         
