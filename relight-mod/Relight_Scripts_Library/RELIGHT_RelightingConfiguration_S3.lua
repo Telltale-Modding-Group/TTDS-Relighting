725b8f7db55d7bd8f80c184286f3bcc064e4fbe1
David Matos <40375502+frostbone25@users.noreply.github.com>
2024-08-09 20:09:05 -0500

    S3 external relight support added

--- relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S3.lua
+++ relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S3.lua
@@ -9,26 +9,285 @@ RELIGHT_GenerateNewINI_Season3 = function(number_configurationVersion)
         {
             ConfigurationVersion = number_configurationVersion,
         },
-
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
-
+        Level_301_AirportExterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_AirportExteriorGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_AirportHangar =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_GarciaDominguezHouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_HardwareStore =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_Junkyard =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_JunkyardDay =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_JunkyardHill =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_VirginiaRailroad =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_VirginiaRoad =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_VirginiaRoadWinter =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_301_Wellington =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
-
+        Level_302_AirportExteriorDusk =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_AirportExteriorGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_GarciaDominguezHouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_JunkyardDay =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_RichmondGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_RichmondStreet =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_RichmondTunnel =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_VirginiaForestBus =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_VirginiaRoad =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_VirginiaRoadCar =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_302_VirginiaUnderpass =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
-
+        Level_303_GarciaDominguezHouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondBackEntrance =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondChurchInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondFactoryExterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondFactoryExteriorBack =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondFactoryInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondGateNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondMedical =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondQuarantineInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondSafeHouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_RichmondSquare =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_VirginiaForestCamp =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_303_VirginiaRailroadNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
-
+        Level_304_BattingCages =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_FlashbackJavi =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_HardwareStoreForestHill =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondApartmentNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondApartments =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondArmory =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondMedical304 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondQuarantineInterior304 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondSafehouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondSquareDawn =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondSquareDusk =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_RichmondWaterTower =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_VirginiaForestBus304 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_304_VirginiaForestCamp304 =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 5 |||||||||||||||||||||||||||||||||||||||
+        Level_305_GarciaDominguezHouse =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondApartments =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondApartmentsRoof =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondChurchInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondGateFinale =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondOverpass =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondSquare =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondSquarePlayground =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondSquareWall =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondSquareWallDusk =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondStreet =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondStreetTile =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_RichmondWaterTower =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_305_VirginiaUnderpass =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
     };
         
     return data;
