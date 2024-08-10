6ebc36dc2a78daa93dc47f989d11e6b68775e922
David Matos <40375502+frostbone25@users.noreply.github.com>
2024-08-09 20:57:48 -0500

    S4 external relight scene lua support

--- relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S4.lua
+++ relight-mod/Relight_Scripts_Library/RELIGHT_RelightingConfiguration_S4.lua
@@ -12,28 +12,196 @@ RELIGHT_GenerateNewINI_Season4 = function(number_configurationVersion)
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 1 |||||||||||||||||||||||||||||||||||||||
+        Level_401_BoardingSchoolBasement = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolDorm = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolDormNight = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolExterior = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolExteriorGate = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolExteriorNight = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_BoardingSchoolInterior = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_ForestShack = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_ForestTile = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_RoadTile = 
+        {
+            RelightSceneLuaFile = nil,
+        },
         Level_401_TrainStation = 
+        {
+            RelightSceneLuaFile = nil,
+        },
+        Level_401_TrainStationNight = 
         {
             RelightSceneLuaFile = nil,
         },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 2 |||||||||||||||||||||||||||||||||||||||
-        Level_402_ =
+        Level_402_BellTower =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolDorm =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolDormNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExteriorDamaged =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExteriorDusk =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExteriorGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExteriorGateNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolExteriorNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_BoardingSchoolInteriorNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_DormRoom =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_ForestCamp =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_ForestTile =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_402_GreenHouse =
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
         },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 3 |||||||||||||||||||||||||||||||||||||||
-        Level_403_ =
+        Level_403_BoardingSchoolDorm =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_BoardingSchoolExteriorBasement =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_BoardingSchoolInterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_ForestBarn =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_ForestCamp =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_RiverBoat =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_RiverShoreline =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_RiverShorelineNight =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_403_TrainTile =
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
         },
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
         --||||||||||||||||||||||||||||||||||||||| EPISODE 4 |||||||||||||||||||||||||||||||||||||||
-        Level_404_ =
+        Level_404_BoardingSchoolDorm =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_BoardingSchoolExterior =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_BoardingSchoolExteriorGate =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_Cave =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_CliffBridge =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_ForestBarn =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_ForestShack =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_FortMcCarrollRanch =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_RiverBoat =
+        {
+            RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
+        },
+        Level_404_RiverShorelineNight =
         {
             RelightSceneLuaFile = RELIGHT_LevelsFolderName .. "/",
         },
